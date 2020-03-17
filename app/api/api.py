8bdca575c383d88b9ef042f759301f0535fd7a65
Mehul Jadav <mehul.jadav@CDSYS.LOCAL>
2020-03-17 11:03:10 +0530

    Revert "RPI-10: Backend is implemented for project api."
    This reverts commit d9a0485d3be8cb2e60b3b59d779a32565370537a.
    
    e

--- app/api/api.py
+++
@@ -1,7 +0,0 @@
-from .endpoints.projects import router as project_router
-from fastapi import APIRouter
-
-router = APIRouter()
-
-# include all project routes to api.py file
-router.include_router(project_router)
